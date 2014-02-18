9d2b6ac9bf42339f073154af5b91f3110fd9ebed
erdemsaid <erdemsaid@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-02-18 23:41:35 +0000

    update to version 2.1
    git-svn-id: https://plugins.svn.wordpress.org/namaz-vakti/trunk@860544 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ include/page.about.php
@@ -0,0 +1,12 @@
+<?php _e('<p>Bu eklenti <a href="http://www.erdemarslan.com/" target="_blank">Erdem Arslan</a> tarafından yazılmıştır. Eklentide kullanılan namaz vakitlerine ait veriler T.C. Diyanet İşleri Başkanlığı\'na aittir. Bu verilerin alınması ve/veya işlenmesi sırasında meydana gelebilecek hatalardan dolayı Erdem Arslan\'ı sorumlu tutamazsınız.</p>
+
+<p><strong>İletişim</strong><br>
+Bu eklenti ile ilgili her türlü görüş öneri ve sorularınız için lütfen <a href="http://www.erdemarslan.com/" target="_blank">www.erdemarslan.com</a> adresini ziyaret ediniz.
+</p>', 'namazvakti'); ?>
+<br>
+<form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
+<input type="hidden" name="cmd" value="_s-xclick">
+<input type="hidden" name="hosted_button_id" value="EHYSTPKQQEUYW">
+<input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_donate_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
+<img alt="" border="0" src="https://www.paypalobjects.com/tr_TR/i/scr/pixel.gif" width="1" height="1">
+</form>
